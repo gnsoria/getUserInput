995931c05a3041432287529893258739247d8a08
Galen Soria <gsoria@study.com>
2018-10-26 16:42:14 -0700

    Add test_getUserInput.py with boilerplate code for testing.

---
+++ test_getUserInput.py
@@ -0,0 +1,25 @@
+import unittest
+
+import getUserInput as ui
+
+class TestInputMethods(unittest.TestCase):
+
+    def test_GetUserInput(self):
+        pass
+    
+    def test_GetYesNo(self):
+        pass
+            
+    def test_GetTrueFalse(self):
+        pass
+            
+    def test_GetInteger(self):
+        pass
+    
+    def my_test_input(return_val="y"):
+        return return_val
+
+if __name__ == "__main__":
+    unittest.main()
+
+    #run with -v for verbose
