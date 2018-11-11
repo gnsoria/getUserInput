25020f9faea66161af000fd3c6d7b12360f4421f
Galen Soria <gnsoria@hush.com>
2018-11-10 21:00:18 -0800

    Added DS_Store to gitignore.

--- test_getUserInput.py
+++ test_getUserInput.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch
 
 import getUserInput as ui
 
@@ -7,18 +8,28 @@ class TestInputMethods(unittest.TestCase):
     def test_GetUserInput(self):
         pass
     
-    def test_GetYesNo(self):
+    def test_GetYesNo_correct_input(self):
+        """This tests correct inputs to the GetYesNo function"""
+        with patch("builtins.input", return_value="y"):
+            self.assertTrue(ui.GetYesNo("Checking y")=="y")
+        
+        with patch("builtins.input", return_value="n"):
+            self.assertTrue(ui.GetYesNo("Checking n")=="n")
+    
+    def test_GetYesNo_wrong_input(self):
+        
         pass
-            
+        #TEST_PROMPT = "Test Prompt"
+
+        #with patch("builtins.input", return_value="x"):
+        #    self.assertFalse(ui.GetYesNo(TEST_PROMPT)=="y")
+
     def test_GetTrueFalse(self):
         pass
             
     def test_GetInteger(self):
         pass
     
-    def my_test_input(return_val="y"):
-        return return_val
-
 if __name__ == "__main__":
     unittest.main()
 
