604d9e7ff64b5104988c14f174d164a07cf567a0
Galen Soria <gnsoria@hush.com>
2019-03-14 21:50:18 -0700

    Align code to PEP8 standards using pycodestyle.

--- getuserinput.py
+++ getuserinput.py
@@ -1,25 +1,33 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 
 """
-This module contains tools for getting input from a user. At any point while getting input, the user may enter "quit", "exit", or "leave" to quit()
+This module contains tools for getting input from a user.
+At any point while getting input, the user may enter "quit",
+  "exit", or "leave" to quit()
 """
+
 _EXIT_WORDS = ["quit", "exit", "leave"]
 
-#Input mode constants
+# Input-mode constants
 INT_ = "INT"
 FLOAT_ = "FLOAT"
-NUM_ = "NUM" #Unlike the others, this doesn't require the underscore, but I kept it to match
+# Unlike the others this doesn't require the underscore but I kept it to match
+NUM_ = "NUM"
 STR_ = "STRING"
-
 
 
 def GetStringChoice(prompt, **kwoptions):
     """
-    Print out the prompt and then return the input as long as it matches one of the options (given as key/value pairs)
+    Print out the prompt and then return the input as long as it matches one
+    of the options (given as key/value pairs)
 
     Example call:
         >>> prompt = "Who is the strongest Avenger?"
-        >>> input_options = {"t":"Thor", "i":"Iron Man", "c":"Captain America", "h":"The Hulk"}
+        >>> input_options = {
+                "t":"Thor",
+                "i":"Iron Man",
+                "c":"Captain America",
+                "h":"The Hulk"}
         >>> response = GetStringChoice(prompt, **input_options)
         Who is the strongest Avenger?
          - 't' for 'Thor'
@@ -30,7 +38,11 @@ def GetStringChoice(prompt, **kwoptions):
         >>> response
         'h'
     Of course, you can also define the kwoptions in the call:
-        >>> response = GetStringChoice("Who is the strongest Avenger?", t="Thor", i="Iron Man", c="Captain America", h="The Hulk")
+        >>> response = GetStringChoice("Who is the strongest Avenger?",
+                t="Thor",
+                i="Iron Man",
+                c="Captain America",
+                h="The Hulk")
         Who is the strongest Avenger?
         - 't' for 'Thor'
         - 'i' for 'Iron Man'
@@ -39,9 +51,13 @@ def GetStringChoice(prompt, **kwoptions):
         h
         >>> response
         'h'
-    
+
     Invalid results are rejected:
-        >>> response = GetStringChoice("Who is the strongest Avenger?", t="Thor", i="Iron Man", c="Captain America", h="The Hulk")
+        >>> response = GetStringChoice("Who is the strongest Avenger?",
+                t="Thor",
+                i="Iron Man",
+                c="Captain America",
+                h="The Hulk")
         Who is the strongest Avenger?
         - 't' for 'Thor'
         - 'i' for 'Iron Man'
@@ -56,31 +72,36 @@ def GetStringChoice(prompt, **kwoptions):
     PAD = len(OPTION_TEMPLATE.format("", ""))
     MAX_LINE_LEN = 60
 
-    #This adjusts the section before the hyphen to be as wide as the longest key.
+    # This adjusts the section before the "-" to be as wide as the longest key
     space = max(map(len, kwoptions))
 
     while True:
         try:
             print(_TruncateAtMax(prompt))
             for key in kwoptions:
-                #The _TruncateAtMax call adjusts the section after the hypen to be no longer than 60
-                #  characters, and indents any overflow lines so that they start at the same place
-                #  as the parent line starts.
+                # The _TruncateAtMax call adjusts the section after the hypen
+                #  to be no longer than 60 characters, and indents any
+                #  overflow lines so that they start at the same place as the
+                #  parent line starts.
                 print(OPTION_TEMPLATE.format(key.ljust(space),
-                    _TruncateAtMax(kwoptions[key], max_len=MAX_LINE_LEN, spacer=space + PAD)))
+                      _TruncateAtMax(
+                          kwoptions[key],
+                          max_len=MAX_LINE_LEN,
+                          spacer=space + PAD)))
 
             user_choice = input()
             if user_choice in kwoptions:
                 return user_choice
             elif user_choice in _EXIT_WORDS:
                 raise SystemExit
-        
+
             print("That wasn't one of the options.",)
         except TypeError:
             print("\n\n")
             raise
         except SystemExit:
             _SysExitMsg()
+
 
 def GetYesNo(prompt):
     """
@@ -103,12 +124,15 @@ def GetYesNo(prompt):
     """
     return GetStringChoice(prompt, y="yes", n="no")
 
+
 def GetTrueFalse(prompt):
     """
-    Calls GetStringChoice and only allows boolean response. Return boolean True or False.
+    Calls GetStringChoice and only allows boolean response.
+    Return boolean True or False.
 
     Example:
-        >>> GetTrueFalse("True or False - Star-Lord was responsible for the team losing on Titan:")
+        >>> GetTrueFalse("True or False - Star-Lord was responsible for"
+                         "the team losing on Titan:")
         True or False - Star-Lord was responsible for the team losing on Titan:
         - 't' for 'True'
         - 'f' for 'False'
@@ -119,6 +143,7 @@ def GetTrueFalse(prompt):
     if GetStringChoice(prompt, t="True", f="False") == "t":
         return True
     return False
+
 
 def GetNumber(prompt, min_opt=-1, max_opt=-1, data_type=NUM_):
     """
@@ -142,37 +167,37 @@ def GetNumber(prompt, min_opt=-1, max_opt=-1, data_type=NUM_):
         5
     - ui.INT_: integers
         >>> my_num = GetNumber("Pick an integer:", 1, 10, ui.INT_)
-        Pick an integer: 
+        Pick an integer:
         (min = 1, max = 10)
         5.0
         >>> my_num
         5
     - ui.FLOAT_: floats
         >>> my_num = GetNumber("Pick an integer:", 1, 10, ui.FLOAT_)
-        Pick an integer: 
+        Pick an integer:
         (min = 1, max = 10)
         5
         >>> my_num
         5.0
     """
     prompt = _TruncateAtMax(prompt)
-    
-    if min_opt==-1.0 and max_opt==-1.0:
-        #no range
+
+    if min_opt == -1.0 and max_opt == -1.0:
+        # No range given
         try:
             print(f"{prompt}")
             num_choice = _AcceptAndValidateNumber()
-        except:
+        except Exception:
             print("\nSomething went wrong...\n")
             raise
     else:
         try:
             print(f"{prompt}")
             num_choice = GetNumberInRange(min_opt, max_opt)
-        except:
+        except Exception:
             print("\nSomething went wrong...\n")
             raise
-    
+
     if data_type == NUM_:
         return num_choice
     elif data_type == FLOAT_:
@@ -180,31 +205,36 @@ def GetNumber(prompt, min_opt=-1, max_opt=-1, data_type=NUM_):
     elif data_type == INT_:
         return int(num_choice)
 
+
 def GetNumberInRange(min_opt, max_opt):
     """
-    Let the user pick a number and return it as whatever data type the user used.
+    Let the user pick a number
+    Return it as whatever data type the user used
     """
     while True:
         try:
             if max_opt < min_opt:
-                #Switch the order if the maximum is less than the minimum. 
-                #  This is done for aesthetics 
+                # Switch the order if the maximum is less than the minimum.
+                # This is done for aesthetics
                 min_opt, max_opt = max_opt, min_opt
             if max_opt == min_opt:
-                return max_opt #ideally, this would raise an error. figure out what to raise here
-            
+                # Ideally, this would raise an error.
+                # TODO: Figure out what to raise here.
+                return max_opt
+
             print(f"(min = {min_opt:,}, max = {max_opt:,})")
             num_choice = _AcceptAndValidateNumber()
 
-            #Check to see if the num_choice is valid in our range
+            # Check to see if the num_choice is valid in our range
             if eval(f"{min_opt}<={num_choice}<={max_opt}"):
-                return num_choice      
+                return num_choice
             print(f"Please pick a number between {min_opt} and {max_opt}.",)
         except SystemExit:
             _SysExitMsg()
-        except:
+        except Exception:
             print("\nSomething went wrong...\n")
             raise
+
 
 def _AcceptAndValidateNumber():
     """
@@ -217,8 +247,8 @@ def _AcceptAndValidateNumber():
             num_choice = input()
             if num_choice in _EXIT_WORDS:
                 _SysExitMsg()
-            
-            #return the corresponding number type
+
+            # Return the corresponding number type
             if num_choice.find(".") == -1:
                 return int(float(num_choice))
             return float(num_choice)
@@ -226,9 +256,10 @@ def _AcceptAndValidateNumber():
             print("Please pick a number.")
         except SystemExit:
             raise
-        except:
+        except Exception:
             print("\nSomething went wrong...\n")
             raise
+
 
 def _TruncateAtMax(str, max_len=80, spacer=1):
     """
@@ -236,19 +267,19 @@ def _TruncateAtMax(str, max_len=80, spacer=1):
     >>>string = "this is a test of the truncate func"
     >>>max_len = 15
     >>>_TruncateAtMax(string, max_len)
-    > this is a test 
+    > this is a test
     > of the truncate
     > func
     """
-    
+
     if len(str) <= max_len - spacer:
         return str
-    
+
     display_str = []
     next_line = ""
     spacing = "\n" + (" " * spacer)
     terms = str.split()
-    
+
     for term in terms:
         if len(next_line + term) < max_len:
             next_line += term + " "
@@ -256,21 +287,22 @@ def _TruncateAtMax(str, max_len=80, spacer=1):
             display_str.append(next_line)
             next_line = term + " "
     else:
-        #adds any stragglers (if next_line != "" but also !< max at the end of terms)
-        display_str.append(next_line) 
+        # Adds any stragglers
+        # (if next_line != "" but also !< max at the end of terms)
+        display_str.append(next_line)
 
     truncated_str = spacing.join(display_str)
-    
+
     return truncated_str[1:] if truncated_str[0] == "\n" else truncated_str[:]
+
 
 def _SysExitMsg(msg="Thanks!"):
     """
-    A consistent process for SystemExit when a user enters one of the _EXIT_WORDS
+    A consistent process for SystemExit when a user enters one of the
+    _EXIT_WORDS
     """
     print(msg)
-    raise SystemExit #raise the SystemExit exception again to exit the program
-
-
+    raise SystemExit  # Raise the SystemExit exception again to exit
 
 
 def main():
@@ -280,31 +312,44 @@ def main():
     _demonstrateGetNumber()
     _demonstrateGetStringChoice()
 
+
 def _demonstrateGetNumber():
     print("""
     Demonstration of GetNumber()
     """)
-    
-    print("Returns {0}\n".format(GetNumber("Step right up and pick a number, any number!")))
 
-    print("Returns {0}\n".format(GetNumber("Only integers this time (decimals will be rounded). Pick any integer!", data_type=INT_)))
-    print("Returns {0}\n".format(GetNumber("Now only an integer between 1 and 10!", 1, 10, data_type=INT_)))
-    print("Returns {0}\n".format(GetNumber("Now pick a float! (root beer not allowed)", data_type=FLOAT_)))
-    print("Returns {0}\n".format(GetNumber("And finally, a float between 1 and 10.", 1, 10, data_type=FLOAT_)))
+    print("Returns {0}\n".format(GetNumber(
+        "Step right up and pick a number, any number!")))
+
+    print("Returns {0}\n".format(GetNumber(
+        "Only integers this time (decimals will be rounded). "
+        "Pick any integer!",
+        data_type=INT_)))
+    print("Returns {0}\n".format(GetNumber(
+        "Now only an integer between 1 and 10!", 1, 10, data_type=INT_)))
+    print("Returns {0}\n".format(GetNumber(
+        "Now pick a float! (root beer not allowed)", data_type=FLOAT_)))
+    print("Returns {0}\n".format(GetNumber(
+        "And finally, a float between 1 and 10.", 1, 10, data_type=FLOAT_)))
     return None
+
 
 def _demonstrateGetStringChoice():
     print("""
     Demonstration of GetStringChoice()
     """)
 
-    print("Returns {0}\n".format(GetStringChoice("What does your mother smell of?", e="elderberries", h="hamster")))
+    print("Returns {0}\n".format(GetStringChoice(
+        "What does your mother smell of?", e="elderberries", h="hamster")))
 
-    print("Returns {0}\n".format(GetYesNo("That was just a little Python humor. Did you enjoy it?")))
+    print("Returns {0}\n".format(GetYesNo(
+        "That was just a little Python humor. Did you enjoy it?")))
 
-    print("Returns {0}\n".format(GetTrueFalse("Is it true that an African swallow could carry a coconut?")))
+    print("Returns {0}\n".format(GetTrueFalse(
+        "Is it true that an African swallow could carry a coconut?")))
 
     return None
+
 
 if __name__ == "__main__":
     main()
